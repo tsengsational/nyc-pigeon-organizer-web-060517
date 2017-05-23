fdfade0d5c2dc9bce666d427ae4111a49e0f5c5f
Jason Tseng <jason.f.tseng@gmail.com>
2017-05-23 20:59:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_names = []
+  pigeon_list = {}
+  data.each do |attribute, item|
+    item.each do |array|
+      array.each do |key, value|
+        pigeon_names << value
+      end
+    end
+  end
+  pigeon_names = pigeon_names.flatten.uniq.delete_if { |e| e.nil? }
+  binding.pry
+
 end
