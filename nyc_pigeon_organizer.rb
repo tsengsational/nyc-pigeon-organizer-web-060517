4a0ffaebb95217c6e871b253ef1a60e56110b9ba
Jason Tseng <jason.f.tseng@gmail.com>
2017-05-24 03:02:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,13 +5,22 @@ def nyc_pigeon_organizer(data)
   pigeon_names = []
   pigeon_list = {}
   data.each do |attribute, item|
-    item.each do |array|
-      array.each do |key, value|
+    item.each do |key, value|
         pigeon_names << value
-      end
     end
   end
   pigeon_names = pigeon_names.flatten.uniq.delete_if { |e| e.nil? }
-  binding.pry
 
+  pigeon_names.each do |name|
+    pigeon_list[name] = Hash.new {|k, v| k[v] = []}
+    data.each do |attribute, item|
+      pigeon_list[name][attribute]
+      item.each do |key, value|
+        value.each do |array|
+          pigeon_list[array][attribute] << key.to_s if array == name
+        end
+      end
+    end
+  end
+  pigeon_list
 end
